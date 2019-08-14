a2376b2180e71db76eeba12cb9d5030b24d47d92
cornehop <corne@honeyside.org>
2019-08-14 11:45:25 +0200

    Merge feature/23-initial-layout into master (#2)
    * 23 Initial CwApp. Removed default stuff. Added Google Fonts.
    
    * Created initial siteheader
    
    * Created body content columns

---
+++ src/Components/Body.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export class Body extends React.Component{
+    render(){
+        return (
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-4">
+                        {/* TODO CREATE LEFT CONTENT */}
+                    </div>
+                    <div className="col-8">
+                        {/* TODO CREATE RIGHT CONTENT */}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
