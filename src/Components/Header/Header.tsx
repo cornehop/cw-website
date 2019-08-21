277dd9301652d21141d90254a2b9bfa66bc8e092
cornehop <corne@honeyside.org>
2019-08-21 14:32:50 +0200

    Merge fix/24-header into master (#8)
    * Fixed header background and made it sticky to top
    
    * Made header set content for homepage

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,26 +1,28 @@
 import React from "react";
 import { HeaderTitleWithSubTitle } from "./HeaderTitleWithSubTitle";
 import "../../Style/CwAppHeader.css";
-import { HeaderFooter } from "./HeaderFooter";
 import Background from "../../Assets/banner_image.png";
+import { Container, Row, Col } from "react-bootstrap";
 
 var style = {
     backgroundImage: "url(" + Background + ")"
 }
 
-export class Header extends React.Component{
+interface HeaderProps{
+    headerClick: () => void;
+}
+
+export class Header extends React.Component<HeaderProps>{
     render(){
         return (
-            <div style={style} className="container-fluid cw-header">
-                <div className="row cw-header-titles">
-                    <div className="col-12">
-                        <HeaderTitleWithSubTitle />
-                    </div>
-                    <div className="col-12 cw-header-footer">
-                        <HeaderFooter />
-                    </div>
-                </div>
-            </div>
+            <Container fluid className="cw-header sticky-top">
+                <Row>
+                    <Col xs={12} md={6} className="cw-header-left">
+                        <HeaderTitleWithSubTitle headerClick={this.props.headerClick} />
+                    </Col>
+                    <Col xs={0} md={6} className="cw-header-right" style={style}></Col>
+                </Row>
+            </Container>
         );
     }
 }
--- src/CwApp.tsx
+++ src/CwApp.tsx
@@ -2,13 +2,52 @@ import React from "react";
 import "./Style/CwApp.css";
 import { Header } from "./Components/Header/Header";
 import { Body } from "./Components/Body";
+import { PageTitles } from "./Constants/PageTitles";
+import { HomePage } from "./Components/Pages/HomePage";
+import { ArchivePage } from "./Components/Pages/ArchivePage";
 
-export class CwApp extends React.Component{
+interface CwAppState{
+    mainCurrentPage: string,
+    mainTitle: string,
+    mainContent: JSX.Element
+}
+
+export class CwApp extends React.Component<{}, CwAppState>{
+    constructor(props: {}){
+        super(props);
+
+        this.state = {
+            mainCurrentPage: "home",
+            mainTitle: PageTitles.HomePage,
+            mainContent: <HomePage />
+        }
+    }
+
+    setContent(page: string){
+        if (page === "home"){
+            this.setState({
+                mainCurrentPage: page,
+                mainTitle: PageTitles.HomePage,
+                mainContent: <HomePage />
+            })
+        }
+        else if (page === "archive"){
+            this.setState({
+                mainCurrentPage: page,
+                mainTitle: PageTitles.ArchivePage,
+                mainContent: <ArchivePage />
+            })
+        }
+    }
+
     render(){
         return (
             <React.Fragment>
-                <Header />
-                <Body />
+                <Header headerClick={() => this.setContent("home")} />
+                <Body mainContent={this.state.mainContent}
+                      mainTitle={this.state.mainTitle}
+                      mainCurrentPage={this.state.mainCurrentPage}
+                      updateContent={(page: string) => this.setContent(page)} />
             </React.Fragment>
         );
     }
